578219c287452583a829a4c61f679b6ab4861936
nschell <nickschell@gmail.com>
2014-02-28 11:56:08 -0500

    Second Commit

---
+++ SecureMessageRepository/src/Main/GUI/OpenSecureDocument.java
@@ -0,0 +1,15 @@
+package Main.GUI;
+
+import javax.swing.*;
+
+/**
+ * Created by Administrator on 2/28/14.
+ */
+public class OpenSecureDocument {
+    private JPanel panel1;
+    private JTextPane secureMessageRepositoryTextPane;
+    private JButton exitProgramButton;
+    private JButton saveButton;
+    private JButton backButton;
+    private JButton deleteMessageButton;
+}
---
+++ SecureMessageRepository/src/Test/TestOpenMessage.java
@@ -0,0 +1,7 @@
+package Test;
+
+/**
+ * Created by Administrator on 2/28/14.
+ */
+public class TestOpenMessage {
+}
